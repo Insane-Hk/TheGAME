b1a038361572e454b17f494ca64c44c17fcd9574
Insane-Hk <jonathan.triclin@gmail.com>
2019-04-16 13:30:39 +0200

    multiplayer + bonuses + other....

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -2,15 +2,17 @@ $(function () {
     var socket = io();
 
     var global = {
+        FPS: 0,
+
         KEY_UP: 38,
         KEY_DOWN: 40,
         KEY_LEFT: 37,
         KEY_RIGHT: 39,
 
-        move_ratio: 2.5,
+        move_ratio: 5,
 
-        gameWidth: 2500,
-        gameHeight: 2500,
+        gameWidth: 5000,
+        gameHeight: 5000,
 
         screenWidth: window.innerWidth,
         screenHeight: window.innerHeight,
@@ -19,38 +21,82 @@ $(function () {
         yoffset: -0,
 
         lineColor: "#000",
+        backgroundColor: "#222222",
+
+        mouseCoord: {
+            x: 0,
+            y: 0,
+        },
     };
 
     var GameArea = {
         canvas: $("#game-canvas"),
         graph: null,
 
-        resizeCanvas: function (canvas, w, h) {
+        resizeCanvas: function (canvas, w, h) 
+        {
             var c = canvas[0]
             c.width = w;
             c.height = h
         },
 
-        resize: function () {
+        resize: function () 
+        {
             global.screenWidth = window.innerWidth;
             global.screenHeight = window.innerHeight;
 
             this.resizeCanvas(this.canvas, global.screenWidth, global.screenHeight);
         },
 
-        drawCircle: function (centerX, centerY, radius, color) {
+        getDistance: function (p1, p2) {
+            return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)) - p1.radius - p2.radius;
+        },
+
+        getMouseCoord: function(evt)
+        {
+            var rect = document.getElementById("game-canvas").getBoundingClientRect();
+            return {
+                x: evt.clientX - rect.left,
+                y: evt.clientY - rect.top
+            };
+        },
+
+        drawCircle: function (centerX, centerY, radius, color) 
+        {
             this.graph.beginPath();
             this.graph.globalAlpha = 1;
             this.graph.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-
             this.graph.fillStyle = color;
             this.graph.fill();
+            this.graph.strokeWidth = 5;
+            this.graph.strokeStyle = "white";
+            this.graph.stroke();
         },
 
-        draw_grid: function () {
+        drawPolygon: function (ctx, x, y, radius, sides, startAngle, anticlockwise) 
+        {
+            if (sides < 3) return;
+
+            var a = (Math.PI * 2)/sides;
+            a = anticlockwise?-a:a;
+            ctx.save();
+            ctx.translate(x,y);
+            ctx.rotate(startAngle);
+            ctx.moveTo(radius,0);
+            
+            for (var i = 1; i < sides; i++) {
+                ctx.lineTo(radius*Math.cos(a*i),radius*Math.sin(a*i));
+            }
+
+            ctx.closePath();
+            ctx.restore();
+        },
+
+        draw_grid: function () 
+        {
             this.graph.beginPath();
 
-            this.graph.fillStyle = "#222222";
+            this.graph.fillStyle = global.backgroundColor;
             this.graph.fillRect(0, 0, global.screenWidth, global.screenHeight);
 
             this.graph.lineWidth = 1;
@@ -70,24 +116,120 @@ $(function () {
             this.graph.stroke();
         },
 
-        draw_player: function() {
+        draw_bonus: function()
+        {
+            global.b_datas.forEach(object => {
+                this.graph.beginPath();
+                this.graph.globalAlpha = 1;
+
+                this.drawPolygon(this.graph, object.x - global.p_datas.x + global.screenWidth / 2, object.y - global.p_datas.y + global.screenHeight / 2, 
+                    25, 8, -Math.PI/2);
+
+                this.graph.fillStyle = "rgb(" + object.color.r + ", " + object.color.g + ", " + object.color.b + ")";
+                this.graph.fill();
+                this.graph.stroke();
+
+                this.graph.font = "12px Arial";
+                this.graph.textAlign = "center";
+                this.graph.fillText("ID : " + object.id, 
+                    object.x - global.p_datas.x + global.screenWidth / 2, 
+                    object.y - global.p_datas.y + global.screenHeight / 2 + 50);
+                /**/
+            });
+        },
+
+        draw_player: function() 
+        {
             this.drawCircle(global.screenWidth / 2, global.screenHeight / 2, 20, 
                 "rgb(" + global.p_datas.color.r + ", " + global.p_datas.color.g + ", " + global.p_datas.color.b + ")");
 
-            this.graph.font = "18px Arial";
+            this.graph.font = "16px Arial";
             this.graph.textAlign = "center";
-            this.graph.fillText(global.p_datas.username, global.screenWidth / 2, global.screenHeight / 2 + 50);
+            this.graph.fillText(global.p_datas.username, global.screenWidth / 2, global.screenHeight / 2 + 40);
 
-            this.graph.font = "18px Arial";
+            
+            this.graph.font = "13px Arial";
             this.graph.textAlign = "center";
-            this.graph.fillText("{x: " + global.p_datas.x + " - y: " + global.p_datas.y + "}", global.screenWidth / 2, global.screenHeight / 2 + 80);
+            this.graph.fillText("{x: " + global.p_datas.x + " - y: " + global.p_datas.y + "}", global.screenWidth / 2, global.screenHeight / 2 + 60);
+            /**/
+        },
+
+        draw_oplayers: function()
+        {
+            global.op_datas.forEach(player => {
+                if (player.id !== global.p_datas.id && player.username !== "#UNKNOWN")
+                {
+                    this.drawCircle(player.x - global.p_datas.x + global.screenWidth / 2, player.y - global.p_datas.y + global.screenHeight / 2, 20, 
+                        "rgb(" + player.color.r + ", " + player.color.g + ", " + player.color.b + ")");
+
+                    this.graph.font = "14px Arial";
+                    this.graph.textAlign = "center";
+                    this.graph.fillText(player.username, player.x - global.p_datas.x + global.screenWidth / 2, 
+                        player.y - global.p_datas.y + global.screenHeight / 2 + 35);
+                }
+            });
+        },
+
+        draw_mouseCursor: function()
+        {
+            this.drawCircle(global.mouseCoord.x, global.mouseCoord.y, 10, 
+                "rgb(" + global.p_datas.color.r + ", " + global.p_datas.color.g + ", " + global.p_datas.color.b + ")");
+
+            this.graph.strokeWidth = 2;
+
+            this.graph.moveTo(global.screenWidth/2, global.screenHeight/2);
+            this.graph.lineTo(global.mouseCoord.x, global.mouseCoord.y);
+
+            this.graph.strokeStyle = "rgb(" + global.p_datas.color.r + ", " + global.p_datas.color.g + ", " + global.p_datas.color.b + ")";
+            this.graph.stroke();
+        },
+
+        handle_bonusGathering: function()
+        {
+            global.b_datas.forEach(object => {
+                if (this.getDistance({x: global.p_datas.x, y: global.p_datas.y, radius: 20}, {x: object.x, y: object.y, radius: 25}) < 10)
+                {
+                    var bool = false;
+
+                    global.p_datas.bonus.forEach(bonus_ => {
+                        if (bonus_.id === object.id)
+                        {
+                            bool = true;
+                        }
+                    });
+
+                    if (!bool)
+                    {
+                        console.log("[TheGAME] [client] - Bonus récupéré (ID: " + object.id + ")");
+                        socket.emit("BonusGathered", {bonus: object})
+                        global.p_datas.bonus.push(object);
+                    }               
+                }
+            });
         },
 
         game_loop: function () {
             global.animLoopHandle = window.requestAnimFrame(GameArea.game_loop);
 
+            if ((global.p_datas.x < 10 || global.p_datas.x > (global.gameWidth - 10)) || (global.p_datas.y < 10 || global.p_datas.y > (global.gameHeight - 10)))
+            {
+                global.backgroundColor = "#A00000";
+            }
+            else
+            {
+                global.backgroundColor = "#222222";
+            }
+
             GameArea.draw_grid();
+
+            GameArea.draw_bonus();
+
+            GameArea.draw_oplayers();
             GameArea.draw_player();
+
+            GameArea.draw_mouseCursor();
+
+            GameArea.handle_bonusGathering();
         },
 
         move: function (direction) {
@@ -129,13 +271,38 @@ $(function () {
                     break;
             }
 
+            socket.emit("UpdateCoords", {x: global.p_datas.x, y: global.p_datas.y});
+
             //log("[TheGAME] [client] - Coordonnées {x: " + global.p_datas.x + ", y: " + global.p_datas.y + "}");
+        },
+
+        populate_playerBoard: function()
+        {
+            /*
+
+            $(".players-list .players").html("");
+
+            global.op_datas.forEach(player => {
+                $(".players-list .players").append(`
+                    <div class="row mb-2 mx-1">
+                        <span class="mr-auto">${player.username}</span>
+                        <span class="ml-auto"></span>
+                    </div>
+                `);
+            });
+
+            */
+
+            $(".players-list h6").text(`${global.op_datas.length} JOUEURS - ${global.b_datas.length} BONUS`);
+            $(".players-list").show();
         },
 
         start: function () {
             this.canvas.show();
             this.graph = this.canvas[0].getContext("2d");
             this.resize();
+
+            this.populate_playerBoard();
 
             this.game_loop();
         },
@@ -145,9 +312,23 @@ $(function () {
         log("[TheGAME] [client] - Changement de vue Lobby => Jeu");
 
         global.p_datas = datas.p_datas;
+        global.op_datas = datas.op_datas;
+        global.b_datas = datas.b_datas;
 
         hideLobby();
         GameArea.start();
+    });
+
+    socket.on("UpdatePlayers", function(datas)
+    {
+        global.op_datas = datas.op_datas;
+
+        GameArea.populate_playerBoard();
+    });
+
+    socket.on("UpdateBonuses", function(datas)
+    {
+        global.b_datas = datas.b_datas;
     });
 
     function hideLobby() {
@@ -168,18 +349,22 @@ $(function () {
         $("body").css("background-color", "#222222");
     }
 
-    function getMousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-            x: evt.clientX - rect.left,
-            y: evt.clientY - rect.top
-        };
-    }
-
     $(".play-button").click(function () {
-        socket.emit("PlayGame", {
-            username: $(".p_username").val()
-        });
+        if ($(".p_username").val().length !== 0)
+        {
+            socket.emit("PlayGame", {
+                username: $(".p_username").val()
+            });
+        }
+        else
+        {
+            log("[TheGAME] [client] - Vous devez définir un nom d'utilisateur !")
+        }
+    });
+
+    $("#game-canvas").on("mousemove", function(event)
+    {
+        global.mouseCoord = GameArea.getMouseCoord(event);
     });
 
     $(document).keydown(function (event) {
--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,15 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var global = {
+    max_gameWidth: 5000,
+    max_gameHeight: 5000,
+
+    max_bonuses: 40,
+};
+
 var Players = [];
+var Bonuses = [];
 
 app.use(express.static(__dirname + '/../client'));
 
@@ -46,16 +54,47 @@ function GenerateRandomColor()
     return {r: r, g: g, b: b}
 }
 
+function GenerateRandomCoords()
+{
+    var x = Math.floor(Math.random() * global.max_gameWidth);
+    var y = Math.floor(Math.random() * global.max_gameHeight);
+
+    return {x: x, y: y};
+}
+
+function GenerateBonuses()
+{
+    for (var i = 0; i < global.max_bonuses; i++)
+    {
+        var position = GenerateRandomCoords();
+
+        var bonus_datas = {
+            id: i,
+            type: "Bonus",
+            color: GenerateRandomColor(),
+            x: position.x,
+            y: position.y,
+        };
+
+        Bonuses.push(bonus_datas);
+    }
+
+    console.log("[TheGAME] - `Bonus` generated !");
+}
+
 io.on("connection", function(socket) {
     console.log("[TheGAME] - USER (" + socket.id + ") connected");
     LogToPlayer(socket, "Connexion au serveur validée (ID : " + socket.id + ")");
+
+    var p_Position = GenerateRandomCoords()
     
     Players.push({
         id: socket.id, 
         username: "#UNKNOWN",
         color: GenerateRandomColor(),
-        x: 0.0,
-        y: 0.0,
+        bonus: [],
+        x: p_Position.x,
+        y: p_Position.y,
     });
 
     LogToPlayer(io, "Connexion de l'utilisateur " + socket.id + " (Joueurs : " + Players.length + ").");
@@ -63,6 +102,10 @@ io.on("connection", function(socket) {
     socket.on("disconnect", function() {
         console.log('[TheGAME] - USER (' + socket.id + ') disconnected');
         Players.splice(GetPlayerIndex(socket.id));
+
+        io.emit("UpdatePlayers", {
+            op_datas: Players
+        })
 
         LogToPlayer(io, "Déconnexion de l'utilisateur " + socket.id + " (Joueurs : " + Players.length + ").");
     });
@@ -76,11 +119,49 @@ io.on("connection", function(socket) {
         UpdatePlayerData(p_index, "username", datas.username)
 
         socket.emit("EnterGameScreen", {
-            p_datas: Players[p_index]
+            p_datas: Players[p_index],
+            op_datas: Players,
+            b_datas: Bonuses,
         });
+
+        io.emit("UpdatePlayers", {
+            op_datas: Players
+        })
+    });
+
+    socket.on("UpdateCoords", function(datas)
+    {
+        var p_index = GetPlayerIndex(socket.id);
+
+        UpdatePlayerData(p_index, "x", datas.x);
+        UpdatePlayerData(p_index, "y", datas.y);
+
+        io.emit("UpdatePlayers", {
+            op_datas: Players
+        })
+    });
+
+    socket.on("BonusGathered", function(datas)
+    {
+        var p_index = GetPlayerIndex(socket.id);
+
+        Players[p_index].bonus.push(datas.bonus);
+
+        for( var i = 0; i < Bonuses.length; i++){ 
+            if ( Bonuses[i].id === datas.bonus.id) {
+                Bonuses.splice(i, 1); 
+            }
+         }
+
+        io.emit("UpdateBonuses", {
+            b_datas: Bonuses
+        })
     });
 })
 
 http.listen(3000, function() {
     console.log("[TheGAME] - Server is running on port 3000.");
+
+    console.log("[TheGAME] - Generating `Bonus`...");
+    GenerateBonuses();
 });
